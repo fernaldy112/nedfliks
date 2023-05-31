bc29e9f7f787eb29401b7c0740c5089f0d7ada71
fernaldyl <90769501+fernaldy112@users.noreply.github.com>
2023-05-31 11:31:30 +0700

    feat: add header component

---
+++ resources/views/components/header.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    @vite('resources/css/app.css')
+</head>
+<body>
+    <header class="flex items-center justify-between flex-wrap bg-black sm:p-6 p-4 sticky top-0">
+        <div class="flex items-center flex-shrink-0 text-white mr-6">
+          <img src="{{ asset('img/logo.png') }}" alt="Logo" class="sm:w-24 w-16">
+        </div>
+        <div>
+            <img src="{{ asset('img/profile-picture.png') }}" alt="Logo" class="sm:w-10 sm:mr-4 w-6 mr-2 rounded-lg">
+        </div>
+    </header>
+    {{ $slot }}
+</body>
+</html>

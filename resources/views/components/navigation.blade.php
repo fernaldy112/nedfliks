196510982aa9c13872cb9790fd301b60a0cc35d8
fernaldyl <90769501+fernaldy112@users.noreply.github.com>
2023-05-31 11:31:40 +0700

    feat: add navigation component

---
+++ resources/views/components/navigation.blade.php
@@ -0,0 +1,13 @@
+<nav class="flex items-center justify-center w-full bg-black">
+    <div class="text-sm">
+      <a href="#" class="inline-block text-white hover:text-white md:mx-20 sm:mx-10 mx-3 mb-2 font-bebas-neue md:text-lg sm:text-md text-sm">
+        TV Shows
+      </a>
+      <a href="#" class="inline-block text-white hover:text-white md:mx-20 sm:mx-10 mx-3 mb-2 font-bebas-neue md:text-lg sm:text-md text-sm">
+        Movies
+      </a>
+      <a href="#" class="inline-block text-white hover:text-white md:mx-20 sm:mx-10 mx-3 mb-2 font-bebas-neue md:text-lg sm:text-md text-sm">
+        My List
+      </a>
+    </div>
+</nav>

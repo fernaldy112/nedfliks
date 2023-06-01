0c7d3086514b013151b5b9890ac067560287121d
fernaldyl <90769501+fernaldy112@users.noreply.github.com>
2023-06-01 23:32:22 +0700

    feat: add horizontal scrollable cards skeleton

---
+++ resources/views/components/horizontal-cards.blade.php
@@ -0,0 +1,14 @@
+<section class="flex flex-nowrap overflow-x-auto snap-x snap-mandatory gap-5 px-5 py-5 no-scrollbar lg:grid lg:grid-cols-4">
+    <div class="bg-purple-400 p-5 rounded flex-none w-[250px] snap-center snap-always shadow-lg">
+        <p>Content goes here</p>
+    </div>
+    <div class="bg-blue-400 p-5 rounded flex-none w-[250px] snap-center snap-always" shadow-lg>
+        <p>Content goes here</p>
+    </div>
+    <div class="bg-green-400 p-5 rounded flex-none w-[250px] snap-center snap-always shadow-lg">
+        <p>Content goes here</p>
+    </div>
+    <div class="bg-red-400 p-5 rounded flex-none w-[250px] snap-center snap-always shadow-lg">
+        <p>Content goes here</p>
+    </div>
+</section>

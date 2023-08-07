349bf88d1d4a46aacef54b357d991946aa95805e
fernaldyl <90769501+fernaldy112@users.noreply.github.com>
2023-08-07 16:43:15 +0700

    feat: homepage responsiveness

--- resources/views/components/horizontal-cards.blade.php
+++ resources/views/components/horizontal-cards.blade.php
@@ -1,8 +1,8 @@
 <div class="">
-    <h2 class="text-white px-5 text-4xl font-bold mt-12">{{ $title }}</h2>
+    <h2 class="text-white px-5 text-xl sm:text-4xl font-bold mt-12">{{ $title }}</h2>
     <section class="flex flex-nowrap overflow-x-auto snap-x snap-mandatory gap-5 px-5 py-5 no-scrollbar xl:grid 4xl:grid-cols-8 3xl:grid-cols-7 2.5xl:grid-cols-6 1.5xl:grid-cols-5 xl:grid-cols-4">
         @foreach ($shows as $show)
-            <div class="rounded flex-none w-64 h-96 snap-center snap-always shadow-lg">
+            <div class="rounded flex-none w-1/2 sm:w-64 snap-center snap-always shadow-lg border aspect-2/3">
                 <a href="/show/{{ $show["id"] }}" class="block bg-center bg-no-repeat bg-cover bg-blend-multiply w-full h-full" style="background-image: url('{{ $show['poster_url'] }}');"></a>
             </div>
         @endforeach

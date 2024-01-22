245d497929fb363935852deed701c0803d5945fc
fernaldyl <90769501+fernaldy112@users.noreply.github.com>
2024-01-22 17:56:37 +0700

    feat: routes

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,8 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ShowController;
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\AuthenticationController;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,8 +17,21 @@ use App\Http\Controllers\ShowController;
 |
 */
 
-Route::get('/', [HomeController::class, "homepage"]);
-Route::get('/tv_shows', [HomeController::class, "tv_shows"]);
-Route::get('/movies', [HomeController::class, "movies"]);
+Route::get('/', [HomeController::class, "homepage"])->middleware("auth");
+Route::get('/tv_shows', [HomeController::class, "tv_shows"])->middleware("auth");
+Route::get('/movies', [HomeController::class, "movies"])->middleware("auth");
 
-Route::get('/show/{show}', [ShowController::class, "show_detail"]);
+Route::get('/show/{show}', [ShowController::class, "show_detail"])->middleware("auth");
+Route::get('/my_list', [ShowController::class, "my_list"])->middleware("auth");
+Route::post('/my_list/{show}', [ShowController::class, "update_my_list"])->middleware("auth");
+
+Route::get('/login', [AuthenticationController::class, "login_page"])->name("login");
+Route::get('/register', [AuthenticationController::class, "register_page"]);
+Route::get('/change_password', [AuthenticationController::class, "change_password_page"])->middleware("auth");
+Route::get('/profile', [ProfileController::class, "profile_page"])->middleware("auth");
+
+Route::post('/login', [AuthenticationController::class, "login"]);
+Route::post('/register', [AuthenticationController::class, "register"]);
+Route::post('/change_password', [AuthenticationController::class, "change_password"])->middleware("auth");
+Route::post('/logout', [AuthenticationController::class, "logout"])->middleware("auth");
+
